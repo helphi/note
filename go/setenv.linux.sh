0cfb03670f93d4105f073a2a7c79c091928cfc09
helphi <helphi.he@qq.com>
2015-10-03 12:44:10 +0800

    add main.go

---
+++ go/setenv.linux.sh
@@ -0,0 +1,4 @@
+export GOROOT=/usr/local/go/
+export GOPATH=$HOME/gopath
+export PATH=$PATH:$GOROOT/bin
+echo "go run main.go | go run --work main.go | go build main.go"
--- go/setenv.windows.bat
+++ go/setenv.windows.bat
@@ -1,4 +1,5 @@
 set GOROOT=D:\dev\go1.4.2.windows-amd64\go
+set GOPATH=%HOMEDRIVE%%HOMEPATH%\gopath
 set Path=%Path%;%GOROOT%\bin
-echo "go run goos.go"
+echo "go run main.go | go run --work main.go | go build main.go"
 cmd
