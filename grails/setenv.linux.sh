cf2fc5b9ab911e0b4dfee1cbffe9384e5ed95320
helphi <helphi.he@qq.com>
2015-10-08 22:58:12 +0800

    init grails

---
+++ grails/setenv.linux.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/d/dev/jdk-7u80-windows-x64/jdk1.7.0_80
+export GRAILS_HOME=/d/dev/grails-2.4.4
+export PATH=$PATH:$GRAILS_HOME/bin:$JAVA_HOME/bin
---
+++ grails/setenv.windows.bat
@@ -0,0 +1,4 @@
+set JAVA_HOME=D:\dev\jdk-7u80-windows-x64\jdk1.7.0_80
+set GRAILS_HOME=D:\dev\grails-2.4.4
+set Path=%Path%;%GRAILS_HOME%\bin;%JAVA_HOME%\bin;D:\dev\PortableGit-2.5.2.2-64-bit\cmd
+cmd
