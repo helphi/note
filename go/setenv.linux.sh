4cf2ab68448f0c03c75130ec6c7145b5df2beab5
helphi <helphi.he@qq.com>
2015-10-15 22:06:55 +0800

    update gopath

--- go/setenv.linux.sh
+++ go/setenv.linux.sh
@@ -1,3 +1,3 @@
 export GOROOT=/d/dev/go1.4.2.windows-amd64/go
-export GOPATH=$HOME/gopath
+export GOPATH=$HOME/gopath-1.4.2
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:/d/dev/PortableGit-2.5.2.2-64-bit/mingw64/bin
--- go/setenv.windows.bat
+++ go/setenv.windows.bat
@@ -1,4 +1,4 @@
 set GOROOT=D:\dev\go1.4.2.windows-amd64\go
-set GOPATH=d:%HOMEPATH%\gopath
+set GOPATH=d:%HOMEPATH%\gopath-1.4.2
 set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin;D:\dev\PortableGit-2.5.2.2-64-bit\cmd
 cmd
