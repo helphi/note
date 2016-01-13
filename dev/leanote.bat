efd451fa80c644cfce9571e565fd78a7e86dc7ef
helphi <helphi.he@qq.com>
2016-01-13 21:42:55 +0800

    add username pwd in leanote.bat

--- dev/leanote.bat
+++ dev/leanote.bat
@@ -1,4 +1,5 @@
 set GOROOT=D:\dev\go1.3.1.windows-amd64\go
 set GOPATH=d:%HOMEPATH%\gopath-1.3.1
 set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin;D:\dev\PortableGit-2.5.2.2-64-bit\cmd
+echo http://localhost:9000/admin/abc123/demo@leanote.com/demo@leanote.com
 revel run github.com/leanote/leanote
