aff6cf787d509b41ad2e50ee3bd04c48b561d4d0
hefei <hefei@travelsky.com>
2016-03-29 10:13:41 +0800

    modify start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,11 @@
 http_proxy=http://172.17.18.84:8080 \
 https_proxy=http://172.17.18.84:8080  \
-no_proxy=hub.com docker  \
+no_proxy=hub.com \
+docker  \
 --registry-mirror=http://hub.com  \
 --registry-mirror=https://6pzhi4th.mirror.aliyuncs.com  \
 daemon  \
 --bip=172.172.172.1/24  \
 --dns=172.26.1.251  \
 --insecure-registry=hub.com  \
--D 
+-D
