974d9126885b9e946d9a730ce2d7afff4fae46f7
hefei <hefei@travelsky.com>
2016-03-29 12:59:36 +0800

    add jboss

---
+++ jboss/start-6.4.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=D:\dev\jdk-6u45-windows-x64\jdk1.6.0_45
+set PATH=%JAVA_HOME%\bin;%PATH%
+D:\dev\jboss-eap-6.4\bin\standalone.bat -b=0.0.0.0 -DXDISTHOME="d:\openjaw\xDistributor" -DDISABLE_XML_ODERING=false
