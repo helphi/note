d6dc23696929cc4d68f02915f6371ac46570bc36
hefei <hefei@travelsky.com>
2016-04-07 17:16:49 +0800

    move sh from docker to dev

---
+++ dev/liteide.bat
@@ -0,0 +1,4 @@
+set GOROOT=D:\dev\go1.4.2.windows-amd64\go
+set GOPATH=d:%HOMEPATH%\gopath-1.4.2
+set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin
+start D:\dev\liteidex28.windows-qt4\liteide\bin\liteide.exe
