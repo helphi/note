ac5bd09de6d1e7f7a171df8c8e3a0feb4f559c45
helphi <helphi.he@qq.com>
2016-04-07 21:32:31 +0800

    dev to script

--- dev/idea.bat
+++
@@ -1,4 +0,0 @@
-set GOROOT=D:\dev\go1.4.2.windows-amd64\go
-set GOPATH=d:%HOMEPATH%\gopath-1.4.2
-set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin
-start D:\dev\ideaIC-2016.1.1\bin\idea.exe
--- dev/liteide.bat
+++
@@ -1,2 +0,0 @@
-call go1.3.1.bat
-start D:\dev\liteidex28.windows-qt4\liteide\bin\liteide.exe
