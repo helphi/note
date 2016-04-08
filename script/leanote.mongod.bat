add47e4732d7c3b9adc61126ddf143e7e74fd271
helphi <helphi.he@qq.com>
2016-04-08 21:29:55 +0800

    update leanote mongo start

--- script/leanote.mongod.bat
+++ script/leanote.mongod.bat
@@ -1,1 +1,3 @@
-D:\dev\mongodb-win32-x86_64-3.0.6\bin\mongod.exe --dbpath D:\Users\HeFei\data-mongod
+D:\dev\mongodb-win32-x86_64-2.6.4\mongodb-win32-x86_64-2.6.4\bin\mongod.exe --dbpath d:%HOMEPATH%\mongodb-win32-x86_64-2.6.4
+echo D:\dev\mongodb-win32-x86_64-2.6.4\mongodb-win32-x86_64-2.6.4\bin\mongorestore.exe -h localhost -d leanote --directoryperdb d:%HOMEPATH%\gopath-1.3.1\src\github.com\leanote\leanote\mongodb_backup\leanote_install_data\
+echo D:\dev\mongodb-win32-x86_64-2.6.4\mongodb-win32-x86_64-2.6.4\bin\mongo.exe
