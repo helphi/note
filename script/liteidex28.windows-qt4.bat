bc3bcd63f9a8c1ad5608ffe90df75ab22d8a7d13
hefei <hefei@travelsky.com>
2016-04-08 10:55:36 +0800

    add git in liteide env

--- script/liteidex28.windows-qt4.bat
+++ script/liteidex28.windows-qt4.bat
@@ -1,3 +1,3 @@
 call go1.4.2.windows-amd64.bat
-set PATH=%PATH%;D:\dev\liteidex28.windows-qt4\liteide\bin
+set PATH=%PATH%;D:\dev\liteidex28.windows-qt4\liteide\bin;D:\dev\PortableGit-2.5.2.2-64-bit\cmd
 start D:\dev\liteidex28.windows-qt4\liteide\bin\liteide.exe
