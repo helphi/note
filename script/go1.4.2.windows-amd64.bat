3bb8fb8bb85e1c064477996361daa3c80b8c6dbc
helphi <helphi.he@qq.com>
2016-04-09 19:46:42 +0800

    update bat

--- script/go1.4.2.windows-amd64.bat
+++ script/go1.4.2.windows-amd64.bat
@@ -1,3 +1,3 @@
 set GOROOT=D:\dev\go1.4.2.windows-amd64\go
 set GOPATH=d:%HOMEPATH%\gopath-1.4.2
-set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin;D:\dev\PortableGit-2.5.2.2-64-bit.7z\cmd
+set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin;D:\dev\PortableGit-2.5.2.2-64-bit\cmd
--- script/leanote.bat
+++ script/leanote.bat
@@ -2,4 +2,6 @@ set GOROOT=D:\dev\go1.3.1.windows-amd64\go
 set GOPATH=d:%HOMEPATH%\gopath-1.3.1
 set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin;D:\dev\PortableGit-2.5.2.2-64-bit\cmd
 echo http://localhost:9000/admin/abc123/demo@leanote.com/demo@leanote.com
+echo revel build github.com\leanote\leanote
+echo gdb -silent --args D:\Users\HeFei\gopath-1.3.1\bin\revel.d\github.com\leanote\leanote\github.com\leanote\leanote.exe -importPath github.com\leanote\leanote revel -srcPath github.com\leanote\leanote
 revel run github.com/leanote/leanote
