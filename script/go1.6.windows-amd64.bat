46b2f49fdb92f04cd1580c5122483cbbf0e4a5e1
helphi <helphi.he@qq.com>
2016-04-11 21:37:10 +0800

    go 1.6 sh

--- script/go1.6.windows-amd64.bat
+++ script/go1.6.windows-amd64.bat
@@ -1,3 +1,3 @@
 set GOROOT=D:\dev\go1.6.windows-amd64\go
 set GOPATH=d:%HOMEPATH%\gopath-1.6
-set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin;D:\dev\PortableGit-2.5.2.2-64-bit\cmd
+set Path=%Path%;%GOROOT%\bin;%GOPATH%\bin;D:\dev\PortableGit-2.5.2.2-64-bit\bin
---
+++ script/go1.6.windows-amd64.sh
@@ -0,0 +1,3 @@
+export GOROOT=/d/dev/go1.6.windows-amd64/go
+export GOPATH=$HOME/gopath-1.6
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:/d/dev/PortableGit-2.5.2.2-64-bit/bin
